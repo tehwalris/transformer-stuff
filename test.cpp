987b3b86d53f9d390917d9acac7bce841d4a27e1
Philippe Voinov <philippevoinov@gmail.com>
2023-06-07 21:22:24 +0200

    Print output for multiple "tokens"

--- test.cpp
+++ test.cpp
@@ -24,22 +24,29 @@ int main(int argc, char **argv)
   uint32_t n_hidden = loader.get_hparams()->n_embd;
   std::vector<float> hidden_in(n_hidden);
   std::vector<float> hidden_out_baseline(n_hidden);
-  for (float &v : hidden_in)
-  {
-    v = 15.0f * rand_float_neg_1_1();
-  }
 
   SimpleTransformerLayer *baseline_layer = create_baseline_llama_layer(&loader, 0);
-  baseline_layer->forward(n_hidden, hidden_in.data(), hidden_out_baseline.data());
 
-  for (uint32_t i : {0u, 1u, n_hidden - 2, n_hidden - 1})
+  for (uint32_t i_context = 0; i_context < 10; i_context++)
   {
-    std::cout << "hidden_out_baseline[" << i << "] =" << hidden_out_baseline[i] << std::endl;
+    std::cout << "i_context = " << i_context << std::endl;
+    for (float &v : hidden_in)
+    {
+      v = 15.0f * rand_float_neg_1_1();
+    }
+
+    baseline_layer->forward(n_hidden, hidden_in.data(), hidden_out_baseline.data());
+
+    for (uint32_t i_hidden : {0u, 1u, n_hidden - 2, n_hidden - 1})
+    {
+      std::cout << "hidden_out_baseline[" << i_hidden << "] =" << hidden_out_baseline[i_hidden] << std::endl;
+    }
+    float hidden_out_sum_baseline = 0.0f;
+    for (float v : hidden_out_baseline)
+    {
+      hidden_out_sum_baseline += v;
+    }
+    std::cout << "hidden_out_sum_baseline = " << hidden_out_sum_baseline << std::endl;
+    std::cout << std::endl;
   }
-  float hidden_out_sum_baseline = 0.0f;
-  for (float v : hidden_out_baseline)
-  {
-    hidden_out_sum_baseline += v;
-  }
-  std::cout << "hidden_out_sum_baseline = " << hidden_out_sum_baseline << std::endl;
 }
