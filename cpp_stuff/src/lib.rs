36477b766b4479810d438bc2e7671cc9919ea5d8
Philippe Voinov <philippevoinov@gmail.com>
2023-06-14 21:09:36 +0200

    Move FFI call to other crate

--- cpp_stuff/src/lib.rs
+++ cpp_stuff/src/lib.rs
@@ -3,11 +3,3 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-pub fn do_thing() {
-    unsafe {
-        let fname_base = std::ffi::CString::new("test").unwrap();
-        let mut x = cml_SimpleLlamaModelLoader::new(fname_base.as_ptr());
-        x.destruct();
-    }
-}
--- toy/src/main.rs
+++ toy/src/main.rs
@@ -1,5 +1,13 @@
+fn do_thing() {
+    unsafe {
+        let fname_base = std::ffi::CString::new("test").unwrap();
+        let mut x = cpp_stuff::cml_SimpleLlamaModelLoader::new(fname_base.as_ptr());
+        x.destruct();
+    }
+}
+
 fn main() {
     println!("Hello, world!");
-    cpp_stuff::do_thing();
+    do_thing();
     println!("Goodbye, world!")
 }
