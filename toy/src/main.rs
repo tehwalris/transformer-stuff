a89400bbd19f91f08234876f983401a1edcc809c
Philippe Voinov <philippevoinov@gmail.com>
2023-06-17 14:35:49 +0200

    Load vocab

--- toy/src/main.rs
+++ toy/src/main.rs
@@ -1,3 +1,10 @@
+#![feature(iter_array_chunks)]
+
+use std::io::{BufRead, Seek};
+
+use ggml::format::TensorLoadInfo;
+use half::f16;
+use llm_base::Vocabulary;
 use rand::Rng;
 use rand_distr::StandardNormal;
 
@@ -42,7 +49,46 @@ fn do_thing(model_path: &str) {
     }
 }
 
-fn do_other_thing(model_path: &str) {
+struct VocabEmbeddings {
+    n_hidden: usize,
+    n_vocab: usize,
+    embeddings: Vec<f16>,
+}
+
+impl VocabEmbeddings {
+    fn load_from_ggml<R: BufRead + Seek>(
+        n_hidden: usize,
+        n_vocab: usize,
+        info: &TensorLoadInfo,
+        reader: &mut R,
+    ) -> Self {
+        assert_eq!(info.dims(), &[n_hidden, n_vocab]); // This is in GGML order (contiguous index first)
+        assert_eq!(info.element_type, ggml::ElementType::F16);
+
+        let embeddings: Vec<f16> = info
+            .read_data(reader)
+            .unwrap()
+            .into_iter()
+            .array_chunks()
+            .map(f16::from_le_bytes)
+            .collect();
+        assert_eq!(embeddings.len(), n_hidden * n_vocab);
+
+        Self {
+            n_hidden,
+            n_vocab,
+            embeddings,
+        }
+    }
+
+    fn get_embedding(&self, i: usize) -> &[f16] {
+        assert!(i < self.n_vocab);
+        let offset = i * self.n_hidden;
+        &self.embeddings[offset..offset + self.n_hidden]
+    }
+}
+
+fn load_vocab(model_path: &str) -> (Vocabulary, VocabEmbeddings) {
     let mut loader = llm_base::Loader::<llm_llama::Hyperparameters, _>::new(|_| {});
     let mut file = std::io::BufReader::new(std::fs::File::open(model_path).unwrap());
     ggml::format::load(&mut file, &mut loader).unwrap();
@@ -50,11 +96,17 @@ fn do_other_thing(model_path: &str) {
     let n_hidden = loader.hyperparameters.n_embd;
     let n_vocab = loader.hyperparameters.n_vocab;
 
-    let vocab_embeddings_tensor_info = loader
-        .tensors
-        .get(&"tok_embeddings.weight".to_string())
-        .unwrap();
-    assert_eq!(vocab_embeddings_tensor_info.dims(), &[n_hidden, n_vocab]); // This is in GGML order (contiguous index first)
+    let vocab_embeddings = VocabEmbeddings::load_from_ggml(
+        n_hidden,
+        n_vocab,
+        loader
+            .tensors
+            .get(&"tok_embeddings.weight".to_string())
+            .unwrap(),
+        &mut file,
+    );
+
+    (loader.vocabulary, vocab_embeddings)
 }
 
 fn main() {
@@ -65,8 +117,18 @@ fn main() {
     }
     let model_path = &args[1];
 
-    println!("Hello, world!");
-    do_other_thing(model_path);
+    println!("Loading vocab");
+    let (vocab, vocab_embeddings) = load_vocab(model_path);
+    println!("Done loading vocab");
+    let token_id = 123;
+    println!(
+        "Embedding of token \"{}\" ({}) starts with {:?}",
+        String::from_utf8(vocab.token(token_id).to_vec()).unwrap(),
+        token_id,
+        &vocab_embeddings.get_embedding(token_id)[..10]
+    );
+
+    println!("Doing thing");
     do_thing(model_path);
-    println!("Goodbye, world!")
+    println!("Done doing thing")
 }
